d06da3c4ce8e5d24eb0ffd95aab8f0e4367e40d2
b1ackitty <jieunj0301@gmail.com>
2025-06-30 22:31:13 +0900

    [과제] 3주 차 과제2
    - 캐러셀 기능 구현 완료
    - 키보드 접근성 완료
    - 포커스 링 스타일링 완료

--- week3-homework/scripts/week3-homework.js
+++ week3-homework/scripts/week3-homework.js
@@ -1,17 +1,23 @@
 {
-  const carousel = document.querySelector(".carousel")
-  const contentWrapper = carousel.querySelector(".carousel-container")
-  const contents = contentWrapper.querySelectorAll(".carousel-content")
+  const carousel = document.querySelector('.carousel')
+  const carouselWrapper = carousel.querySelector('.carousel-wrapper')
+  const carouselContainer = carousel.querySelector('.carousel-container')
+  const contents = carouselContainer.querySelectorAll('.carousel-content')
 
   const prevButton = carousel.querySelector('[aria-label^="이전"]')
   const nextButton = carousel.querySelector('[aria-label^="다음"]')
 
   const SELECTED_CLASSNAME = 'is-selected'
+  let selectedFirstIndex = -1
+
+  setButtonState()
+  setTabindexControl()
 
   nextButton.addEventListener('click', () => {
-    const selectedFirstContent = contentWrapper.querySelector('.' + SELECTED_CLASSNAME)
+    if (prevButton.getAttribute('disabled') === '') prevButton.removeAttribute('disabled')
 
-    let selectedFirstIndex = -1
+    const selectedFirstContent = carouselContainer.querySelector('.' + SELECTED_CLASSNAME)
+
     for (let i = 0, l = contents.length; i < l; i++) {
       if (contents.item(i) === selectedFirstContent) {
         selectedFirstIndex = i
@@ -19,17 +25,30 @@
       }
     }
 
-    const firstFirstContentRecInfo = contents.item(selectedFirstIndex).getBoundingClientRect()
-    const nextFirstContentRecInfo = contents.item(selectedFirstIndex + 2).getBoundingClientRect()
-    const distance = nextFirstContentRecInfo.x - firstFirstContentRecInfo.x
-    contentWrapper.style.setProperty('transform', 'translateX(-' + distance + 'px)')
-    // console.log(distance)
+    if (!contents.item(selectedFirstIndex + 4)) nextButton.setAttribute('disabled', '')
+    
+    const carouselWrapperRecInfo = carouselWrapper.getBoundingClientRect()
+    const distance = (carouselWrapperRecInfo.width + 16) * ((selectedFirstIndex + 2) / 2)
+    carouselContainer.style.setProperty('transform', 'translateX(-' + distance + 'px)')
+    
+    selectedFirstContent.classList.remove(SELECTED_CLASSNAME)
+    selectedFirstContent.nextElementSibling.classList.remove(SELECTED_CLASSNAME)
+    contents.item(selectedFirstIndex + 2).classList.add(SELECTED_CLASSNAME)
+    contents.item(selectedFirstIndex + 3).classList.add(SELECTED_CLASSNAME)
+
+    // const carouselIndicator = carousel.querySelector('carousel-indicator')
+    const currentPage = document.querySelector('.current-page')
+    currentPage.textContent = (selectedFirstIndex + 4) / 2
+
+
+    setTabindexControl()
   })
 
   prevButton.addEventListener('click', () => {
-    const selectedFirstContent = contentWrapper.querySelector('.' + SELECTED_CLASSNAME)
+    if (nextButton.getAttribute('disabled') === '') nextButton.removeAttribute('disabled')
 
-    let selectedFirstIndex = -1
+    const selectedFirstContent = carouselContainer.querySelector('.' + SELECTED_CLASSNAME)
+
     for (let i = 0, l = contents.length; i < l; i++) {
       if (contents.item(i) === selectedFirstContent) {
         selectedFirstIndex = i
@@ -37,10 +56,51 @@
       }
     }
 
-    const firstFirstContentRecInfo = contents.item(selectedFirstIndex).getBoundingClientRect()
-    const prevFirstContentRecInfo = contents.item(selectedFirstIndex - 2).getBoundingClientRect()
-    const distance = firstFirstContentRecInfo.x - prevFirstContentRecInfo.x
-    contentWrapper.style.setProperty('transform', 'translateX(-' + distance + 'px)')
-    // console.log(distance)
+    if (!contents.item(selectedFirstIndex - 3)) prevButton.setAttribute('disabled', '')
+    
+    const carouselWrapperRecInfo = carouselWrapper.getBoundingClientRect()
+    const distance = (carouselWrapperRecInfo.width + 16) * ((selectedFirstIndex - 2) / 2)
+    carouselContainer.style.setProperty('transform', 'translateX(-' + distance + 'px)')
+    
+    selectedFirstContent.classList.remove(SELECTED_CLASSNAME)
+    selectedFirstContent.nextElementSibling.classList.remove(SELECTED_CLASSNAME)
+    contents.item(selectedFirstIndex - 1).classList.add(SELECTED_CLASSNAME)
+    contents.item(selectedFirstIndex - 2).classList.add(SELECTED_CLASSNAME)
+
+    // const carouselIndicator = carousel.querySelector('carousel-indicator')
+    const currentPage = document.querySelector('.current-page')
+    currentPage.textContent = (selectedFirstIndex) / 2
+
+
+    setTabindexControl()
   })
+
+  function setButtonState() {
+    let selectedFirstIndex = -1
+
+    for (let i = 0, l = contents.length; i < l; i++) {
+      if (contents.item(i).classList.contains(SELECTED_CLASSNAME)) {
+        selectedFirstIndex = i
+        break
+      }
+    }
+
+    if (selectedFirstIndex === -1) {
+      console.warn('어느 캐러셀 콘텐츠에도 활성 상태를 나타내는 클래스 이름이 없습니다.',)
+    } else if (selectedFirstIndex === 0) {
+      prevButton.setAttribute('disabled', '')
+    } else if (selectedFirstIndex === contents.length - 2) {
+      nextButton.setAttribute('disabled', '')
+    }
+  }
+
+  function setTabindexControl() {
+    contents.forEach((content) => {
+      if (content.classList.contains(SELECTED_CLASSNAME)) {
+        content.querySelector('a').removeAttribute('tabindex')
+      } else {
+        content.querySelector('a').setAttribute('tabindex', '-1')
+      }
+    })
+  }
 }
